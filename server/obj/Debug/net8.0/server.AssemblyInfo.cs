a5e901ac71f67db79065390a984ad09626084057
AdiBhan <abhan03@bu.edu>
2024-10-18 15:12:31 -0400

    Added OAuth Support, Configured Credentials on GCP,  and built '/login' route which opens authentication in the Browser

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c0dc295c42e718d75e8d2b38d29582d3f1e6217")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afcf12ff29deff21f64aacf58212c9fa29fef785")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
