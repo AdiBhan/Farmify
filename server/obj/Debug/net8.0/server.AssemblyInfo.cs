27b3b0f7c04fe42bafe0ebbdde54c0d58e491150
simeongluzman <simeongluzman@gmail.com>
2024-11-15 14:51:38 -0500

    attempting to run backend with new payment controllers

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63dd65eb8eaaa8e0780f9795f973ab84fb37341c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+915260eef0448c65d0478706242a8e50db30eb32")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
