1c0bee18cfa7c3eb8762c0d0b84716d21e4dfde4
simeongluzman <simeongluzman@gmail.com>
2024-11-15 13:03:30 -0500

    Updated bid model for new db schema

--- server/models/Bid.cs
+++ server/models/Bid.cs
@@ -1,26 +1,31 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FarmifyService.models
 {
     public class Bid
     {
-        public int ID { get; set; }
-        public int BuyerID { get; set; }
-        public decimal Amount { get; set; }
-        public DateTime Date { get; set; }
-        public int AuctionID { get; set; }
-
-
-
+        [Key]
+        public string ID { get; set; } // Matches character(36)
         [Required]
-        public Buyer buyer { get; set; }
-
-        
+        [ForeignKey("Buyer")]
+        public string BuyerID { get; set; } // Matches character(36)
         [Required]
-        public Auction auction { get; set; }
+        public decimal Amount { get; set; } // Matches numeric(8, 2)
+        [Required]
+        public DateTime TimeStamp { get; set; } // Matches timestamp without time zone
+        [Required]
+        [ForeignKey("Product")]
+        public long AuctionID { get; set; } // Matches bigint
+        [Required]
+        public decimal Price { get; set; } // Matches numeric
+        [Required]
+        public bool DeliveryStatus { get; set; } // Matches boolean
+        public int? Rating { get; set; } // Matches nullable integer
+
+        // Navigation properties
+        public Buyer Buyer { get; set; }
+        public Product Product { get; set; }
     }
 }
