7468ac3fed2185fbbf218dfe21cb3e9ff683b1ae
simeongluzman <simeongluzman@gmail.com>
2024-11-16 11:26:41 -0500

    added image urls to products table

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c833533a16dc69b79f366656788b34a152d0abec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbdabae3da2d235063af7e4932ace56da19fda7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
