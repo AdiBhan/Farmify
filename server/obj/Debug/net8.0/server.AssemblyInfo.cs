33f5187efab1cb66709c70e02a0f33c6b1e363ca
simeongluzman <simeongluzman@gmail.com>
2024-11-17 18:30:49 -0500

    Test

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7468ac3fed2185fbbf218dfe21cb3e9ff683b1ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a22640b530a63d92ee53765876601f576914298")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
