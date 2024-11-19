410ca2c48a04edb9397f0254eed03427c48eaff6
simeongluzman <simeongluzman@gmail.com>
2024-11-19 09:06:53 -0500

    Merged adi branch

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33f5187efab1cb66709c70e02a0f33c6b1e363ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14e85d6c1b5f23f328af8c755acd3c9e7f69eb42")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
