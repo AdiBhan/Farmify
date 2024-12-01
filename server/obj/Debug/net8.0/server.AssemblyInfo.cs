c34088483522071dc8729ccb4f8185310db22b2a
simeongluzman <simeongluzman@gmail.com>
2024-12-01 13:29:02 -0500

    added progress bar install to npm

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b48c9141846a07c9e76df42017a854227005054c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdc38bc861834fbad851cec0a761d25c61f4ec3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
