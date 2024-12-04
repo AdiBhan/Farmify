c53e88fef9e670b4bb19381eacbce70db2782c77
simeongluzman <simeongluzman@gmail.com>
2024-12-04 09:16:27 -0500

    adjusted readme

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a98492a36cc500b179a252708c2117febe17c9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f73eb11ae74ebbea973eceefeaa9e6c1cf6de9f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
